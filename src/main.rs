a5e3b43a7bf584464db197083b508c487fbf7d7d
Jeremy Rifkin <rifkin.jer@gmail.com>
2020-12-28 14:45:03 -0700

    Updated benchmarking methedology as well as benchmarking driver / result display code. Finally compiling without a wall of warnings!

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ mod utils;
 
 const SEED: u64 = 2222;
 
+#[derive(Clone)]
+#[derive(Debug)]
 struct BenchmarkResult {
 	mean: f64,
 	stdev: f64
@@ -17,12 +19,12 @@ struct BenchmarkResult {
 
 // computes the sample standard deviation of a Vec<u64>
 // mean passed as a parameter to take advantage of pre-computed value
-fn stdev(array: &Vec<u64>, mean: f64) -> f64 {
+fn stdev(array: &Vec<u64>, completed: usize, mean: f64) -> f64 {
 	let mut sum = 0.0;
-	for x in array {
-		sum += (*x as f64 - mean).powi(2);
+	for i in 0..completed {
+		sum += (array[i] as f64 - mean).powi(2);
 	}
-	sum /= (array.len() - 1) as f64;
+	sum /= (completed - 1) as f64;
 	sum.sqrt()
 }
 
@@ -37,7 +39,7 @@ fn destroy_cache() {
 }
 
 // benchmarks a sorting algorithm
-fn bench(sort: fn(&mut [i32]), size: usize, n_tests: usize) -> BenchmarkResult {
+fn bench(sort: fn(&mut [i32]), size: usize, n_tests: usize) -> Option<BenchmarkResult> {
 	// setup tests
 	let mut test_vectors: Vec<Vec<i32>> = vec![vec![0; size]; n_tests];
 	let mut rng = SmallRng::seed_from_u64(SEED);
@@ -48,63 +50,110 @@ fn bench(sort: fn(&mut [i32]), size: usize, n_tests: usize) -> BenchmarkResult {
 	}
 	// run tests
 	let mut results: Vec<u64> = vec![0; n_tests];
+	let mut running_sum = 0.0;
+	let mut completed = 0;
 	for i in 0..n_tests {
 		destroy_cache();
-		let now = Instant::now();
+		let start = Instant::now();
 		sort(&mut test_vectors[i]);
-		results[i] = now.elapsed().as_nanos() as u64;
+		completed += 1;
+		results[i] = start.elapsed().as_nanos() as u64;
+		running_sum += results[i] as f64;
+		if running_sum >= 10e9 {
+			break;
+			//return Option::None;
+		}
 	}
-	// verify validity
-	for i in 0..n_tests {
-		assert!(test_vectors[i].windows(2).all(|slice| slice[0] <= slice[1]));
+	if completed < 30 {
+		return Option::None;
+	}
+	// verify correctness
+	for i in 0..completed {
+		utils::verify_sorted(&test_vectors[i]);
 	}
 	// mean and stdev
-	let mean = results.iter().sum::<u64>() as f64 / n_tests as f64;
-	BenchmarkResult {
+	let mean = running_sum / completed as f64;
+	Option::Some(BenchmarkResult {
 		mean,
-		stdev: stdev(&results, mean)
-	}
+		stdev: stdev(&results, completed, mean)
+	})
 }
 
 fn main() {
-	let algorithms: [(fn(&mut [i32]), &str); 6] = [
+	let algorithms: Vec<(fn(&mut [i32]), String)> = vec![
+		pair!(algos::bubblesort::<i32>),
+		pair!(algos::cocktail_shaker::<i32>),
+		pair!(algos::selectionsort::<i32>),
+		pair!(algos::insertionsort::<i32>),
+		pair!(algos::shell::<i32>),
 		pair!(algos::mergesort_pre_alloc::<i32>),
 		pair!(algos::mergesort_repeated_alloc::<i32>),
 		pair!(algos::mergesort_hybrid::<i32>),
 		pair!(algos::mergesort_in_place_naive::<i32>),
 		pair!(algos::mergesort_in_place::<i32>),
-		pair!(rustsort::<i32>)
+		pair!(algos::heapsort_bottom_up::<i32>),
+		pair!(algos::heapsort_top_down::<i32>),
+		pair!(sort::weird::<i32>),
+		pair!(sort::rustsort::<i32>)
 	];
-	let mut table = Table::new();
-	let mut header = vec![""];
-	for a in algorithms.iter() {
-		header.push(a.1);
-	}
-	table.add_row(Row::new(header.iter().map(|x| Cell::new(x)).collect()));
+	// run tests
+	let mut results = vec![Vec::new(); algorithms.len()];
+	let mut header = vec![String::from("")];
 	let mut test_size = 10;
-	let n_tests = 30;
+	let n_tests = 200;
 	let mut algorithm_enable_flags = vec![true; algorithms.len()];
 	while test_size <= 1_000_000 {
-		let mut row = vec![Cell::new(&commafy(test_size))];
+		header.push(utils::commafy(test_size));
 		for (i, a) in algorithms.iter().enumerate() {
-			println!("{} {}", test_size, a.1);
+			println!("{} {}", utils::commafy(test_size), a.1);
 			if algorithm_enable_flags[i] {
 				let b = bench(a.0, test_size, n_tests);
-				// ns / 1e6 -> ms
-				// *2 for 95% CI
-				let s = format!("{:.5} ± {:.5} ({:.0}%)", b.mean / 1e6, 2.0 * b.stdev / 1e6, (2.0 * b.stdev / 1e6) / (b.mean / 1e6) * 100.0);
-				row.push(Cell::new(&s));
-				// disable algorithm if it was exceedingly slow for this test
-				if b.mean / 1e6 >= 1000.0 {
+				if b.is_none() {
 					algorithm_enable_flags[i] = false;
 				}
+				results[i].push(b);
 			} else {
+				results[i].push(Option::<BenchmarkResult>::None);
+			}
+		}
+		test_size *= 10;
+	}
+	// delimited table
+	for cell in header.iter() {
+		print!("{} ", cell);
+	}
+	print!("\n");
+	for (i, a) in algorithms.iter().enumerate() {
+		print!("{}", a.1);
+		for result in results[i].iter() {
+			if result.is_none() {
+				print!(" -");
+			} else {
+				let result = result.as_ref().unwrap();
+				let v = result.mean / 1e6;
+				print!(" {:.5}", v);
+			}
+		}
+		print!("\n");
+	}
+	// make table
+	let mut table = Table::new();
+	table.add_row(Row::new(header.iter().map(|x| Cell::new(x)).collect()));
+	for (i, a) in algorithms.iter().enumerate() {
+		let mut row = vec![Cell::new(&a.1)];
+		for result in results[i].iter() {
+			if result.is_none() {
 				row.push(Cell::new("-"));
+			} else {
+				let result = result.as_ref().unwrap();
+				let v = result.mean / 1e6;
+				let ci = 2.0 * result.stdev / 1e6;
+				let s = format!("{:.5} ± {:.5} ({:.0}%)", v, ci, ci / v * 100.0);
+				row.push(Cell::new(&s));
 			}
 		}
 		table.add_row(Row::new(row));
-		test_size *= 10;
 	}
 	table.printstd();
-	println!("values in ms; 95% confidence interval displayed");
+	println!("└ Values in ms; 95% confidence interval displayed");
 }
