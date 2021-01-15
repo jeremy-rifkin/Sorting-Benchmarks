0f7aea6386b96caccbf83609c56abe5044f926ba
Jeremy M. Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2021-01-14 23:59:47 -0700

    updated introsort to correctly limit recursion depth and added a tail call recursion optimization

--- src/algos/introsort.rs
+++ src/algos/introsort.rs
@@ -3,25 +3,34 @@ use crate::algos::insertionsort;
 use crate::algos::quicksort;
 use crate::unchecked_tools::SliceUnchecked;
 
+const fn num_bits<T>() -> usize { std::mem::size_of::<T>() * 8 }
+
 pub fn introsort<T: Ord + Copy>(array: &mut [T]) {
-	// max_depth = floor(log_2(array.len())) * 2
-	// floor(log_2(array.len())) = number of times we can bitshift right and still be >= 1
-	// we can build-in the *2 by just checking >= 0
-	// so what we do here to calculate the log is binary shift by 1 each recursion
-	// TODO: is this optimal? or would it be better to just use floats to calculate the log upfront...
-	introsort_step(array, array.len());
+	// max recursion = 2 * floor(log_2(array.len()))
+	introsort_step(array, (num_bits::<usize>() - array.len().leading_zeros() as usize - 1) * 2);
 }
 
-pub fn introsort_step<T: Ord + Copy>(array: &mut [T], max_depth_intermediate: usize) {
-	if array.len() <= 32 {
-		insertionsort(array);
-	} else if max_depth_intermediate == 0 {
-		heapsort::heapsort_bottom_up(array);
-	} else {
-		let pivot = quicksort::partition_end(array);
-		// safety: 0 <= pivot < array.len()
-		let (l, r) = unsafe { array.split_at_unchecked_mut_excl(pivot) };
-		introsort_step(l, max_depth_intermediate >> 1);
-		introsort_step(r, max_depth_intermediate >> 1);
+fn introsort_step<T: Ord + Copy>(mut array: &mut [T], r_height: usize) {
+	// loop creates something along the lines of a tail-call recursion
+	// TODO: no performance difference observed on x86 :/
+	loop {
+		if array.len() <= 32 {
+			insertionsort(array);
+			return;
+		} else if r_height == 0 {
+			heapsort::heapsort_bottom_up(array);
+			return;
+		} else {
+			let pivot = quicksort::partition_end(array);
+			// safety: 0 <= pivot < array.len()
+			let (l, r) = unsafe { array.split_at_unchecked_mut_excl(pivot) };
+			if l.len() < r.len() {
+				introsort_step(l, r_height - 1);
+				array = r;
+			} else {
+				introsort_step(r, r_height - 1);
+				array = l;
+			}
+		}
 	}
 }
